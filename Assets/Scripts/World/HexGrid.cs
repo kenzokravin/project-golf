60189379b6636ccb9136fc5505ae3306c59a6105
kenzokravin <kenzokravin@gmail.com>
2025-02-17 16:34:41 +1100

    Fixed Tile Scaling

--- Assets/Scripts/World/HexGrid.cs
+++ Assets/Scripts/World/HexGrid.cs
@@ -7,12 +7,14 @@ public class HexGrid : MonoBehaviour
     public int mapHeight = 10;
     public float tileSize = 1;
     public GameObject tilePrefab;
+    public GameObject outTilePrefab;
+
     public Camera mainCam;
     
     void Start()
     {
-      
 
+        
 
         MakeMapGrid();
     }
@@ -53,32 +55,87 @@ public class HexGrid : MonoBehaviour
 
             Vector2 camBounds = GetCameraBounds();
 
-            mapWidth = Mathf.FloorToInt(camBounds.x / (tileWidth * 0.75f));
+        
+
+            mapWidth = Mathf.FloorToInt((camBounds.x) / (tileWidth * 0.75f));
             mapHeight = Mathf.FloorToInt(camBounds.y / (tileHeight * 0.75f));
 
+        float widthCutoffCheck = 0;
+        int tileHorizontalNum = 0;
 
 
+        float xStartPositionCoord = 0 * tileSize * Mathf.Cos(Mathf.Deg2Rad * 30);
+        float xEndPositionCoord = 0;
 
-
-            Debug.Log(tileHeight);
-
-
-
-
-
-        Vector3 startPosition = new Vector3((-camBounds.x * .5f) + tileWidth/2, -camBounds.y * .5f, 0);
 
         for (int x = 0; x < mapWidth; x++)
         {
+            float xPos = x * tileSize * Mathf.Cos(Mathf.Deg2Rad * 30);
+
+            widthCutoffCheck = xPos;
+            tileHorizontalNum++;
+            xEndPositionCoord = x * tileSize * Mathf.Cos(Mathf.Deg2Rad * 30);
+        }
+
+
+        float xTotalWidth = xEndPositionCoord - xStartPositionCoord;
+
+        xTotalWidth = xTotalWidth / 2;
+
+        // Calculate the starting X position (shift grid right to center)
+        float startX = -xTotalWidth ; // Shifting right for even spacing
+
+        // Shift left to center the grid
+        // startX = -((gridWidth - tileWidth) * 0.5f);
+
+        Debug.Log("TileNumberHorizontal: " + tileHorizontalNum);
+
+        Debug.Log("Starting X position: " + startX);
+
+
+        Debug.Log(camBounds.x);
+        Debug.Log("mapW: " + mapWidth);
+
+
+        //Debug.Log(tileHeight);
+
+
+
+        Vector3 startPosition = new Vector3(startX, -camBounds.y * .5f, 0);
+
+        for (int x = 0; x < mapWidth; x++)
+        {
+
+
             for(int z = 0; z < mapHeight; z++)
             {
 
+
+
                 Vector3 hexCoords = GetHexCoords(x, z) + startPosition;
 
-                Vector3 position = new Vector3(hexCoords.x, hexCoords.y, 0);
-                GameObject tile = Instantiate(tilePrefab, position, Quaternion.identity);
-                tile.transform.parent = transform;
+                if(hexCoords.x - (tileWidth *0.75f) <= -camBounds.x *.5f || hexCoords.x + (tileWidth * 0.75f) >= camBounds.x * .5f)
+                {
 
+                    Vector3 position = new Vector3(hexCoords.x, hexCoords.y, 0);
+
+                    GameObject tile = Instantiate(outTilePrefab, position, Quaternion.identity);
+                    tile.transform.parent = transform;
+
+
+
+                } else {
+
+
+                    Vector3 position = new Vector3(hexCoords.x, hexCoords.y, 0);
+
+                    GameObject tile = Instantiate(tilePrefab, position, Quaternion.identity);
+                    tile.transform.parent = transform;
+
+
+
+
+                }
             }
 
         }
